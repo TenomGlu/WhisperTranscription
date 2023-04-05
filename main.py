3981e52301afd5771b2c38fe0ff8616ec633b532
Tenom <76705625+TenomGlu@users.noreply.github.com>
2023-04-04 20:12:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 import subprocess
 import whisper_timestamped as whisper
-from pathlib import Path
 
-# Creating a variable that references the path of the mp4 being transcribed
-video_location = 'Consent .mp4'
+consentVideo = 'Consent .mp4'
+ConsentTranscript = 'consent_transcript.txt'
 
-# Creating a variable that references the path of the mp3 converted file
-video_transcriptions = 'transcriptaudio.mp3'
+ContraceptionVideo = 'Contraception.mp4'
+ContraceptionTranscript = 'contraception_transcript.txt'
 
-# Load the whisper model
+currentVideo = consentVideo
+currentTranscript = ConsentTranscript
+
 model = whisper.load_model("base")
 
-video_in = video_location
-audio_out = video_location
+video_in = currentVideo
+audio_out = currentVideo
 
 ffmpeg_cmd = f"ffmpeg -i {video_in} -vn -c:a libmp3lame -b:a 192k {audio_out}"
 
 subprocess.run(["ffmpeg", "-i", video_in, "-vn", "-c:a", "libmp3lame", "-b:a", "192k", audio_out])
 
 result = model.transcribe(audio_out)
+newDict = {}
+with open(currentTranscript, 'w') as f:
+    iterator = 0
+    while iterator < len(result['segments']):
+        lineInMemory = (result['segments'][iterator])
+        if lineInMemory['start'] > 60:
+            minutes = int(lineInMemory['start']/60)
+            seconds = int(lineInMemory['start'] % 60)
+            if seconds < 10:
+                seconds = "0" + str(seconds)
+            f.write("(" + str(minutes) + ":" + str(seconds) + "s" + ")" + ":")
+            f.write(lineInMemory['text'])
+            f.write("\n")
+        else:
+            minutes = int(lineInMemory['start'] / 60)
+            seconds = int(lineInMemory['start'] % 60)
+            if seconds < 10:
+                seconds = "0" + str(seconds)
+            f.write("(" + str(minutes) + ":" + str(seconds) + "s" + ")" + ":")
+            f.write(lineInMemory['text'])
+            f.write("\n")
+        iterator += 1
 
-# Write to a text file
-with open('transcript.txt', 'w') as f:
-    f.write(str(result))
 f.close()
-
-# Still need to add parsing feature so that transcription can be copy-pasted into a text file in a neat format only one time with only
-# minor changes necessary
